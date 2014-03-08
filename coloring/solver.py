532f4a0ad2443ab73f76f37a58f0ce14883fafad
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-03-08 14:29:27 +0800

    Add graph connect test

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,18 +1,89 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import numpy as np
+
 
 def coloring(edges):
     print edges
     return range(0, len(edges) + 1)
 
-def connectSplit(edges):
-    def split(edges):
-        return (edges,)
-    ret = split(edges)
-    if len(ret) == 1:
-        return ret[0]
+
+def buildAdjMat(edges):
+    n = max((max(e) for e in edges)) + 1
+    mat = [[0 for i in xrange(n)] for j in xrange(n)]
+    for e in edges:
+        mat[e[0]][e[1]] = 1
+        mat[e[1]][e[0]] = 1
+    return np.matrix(mat)
+
+
+def adjMatrixPower(mat, n):
+    if n == 1:
+        return mat
+    if n % 2 == 0:
+        mat = adjMatrixPower(mat, n / 2)
+        return mat * mat
     else:
-        return (split[i] for i in ret)
+        return mat * adjMatrixPower(mat, n - 1)
+
+
+def logicMatMul(a, b):
+    n = a.shape[0]
+    C = a * b
+    c = np.zeros((n, n))
+    for i in xrange(n):
+        for j in xrange(n):
+            if C[i, j] != 0:
+                c[i, j] = 1
+    return c
+
+
+def logicMatAdd(a, b):
+    n = a.shape[0]
+    new = False
+    for i in xrange(n):
+        for j in xrange(n):
+            if a[i, j] == 0 and b[i, j] != 0:
+                new = True
+                a[i, j] = 1
+    return a, new
+
+
+def adjMatrixAccu(mat, n):
+    matN = mat
+    acc = matN
+    for i in xrange(1, n):
+        matN = logicMatMul(matN, mat)
+        acc, newer = logicMatAdd(acc, matN)
+        if newer == False:
+            break
+    return acc
+
+
+def connectSplit(edges):
+    # 连通性分析代码
+    # 没有实际作用
+    mat = buildAdjMat(edges)
+    n = mat.shape[0]
+    con = adjMatrixAccu(mat, n)
+    initflag = -1
+    label = [initflag for i in xrange(n)]
+    label[0] = 0
+    cnt = 1
+    start = 0
+    while 1:
+        for i in xrange(n):
+            if con[start, i] == 1 and label[i] == initflag:
+                label[i] = label[start]
+                cnt += 1
+            else:
+                nextstart = i
+        if cnt >= n - 1:
+            break
+        start = nextstart
+        label[start] = start
+    return label
 
 
 def solve_it(input_data):
@@ -54,4 +125,3 @@ if __name__ == '__main__':
         print solve_it(input_data)
     else:
         print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)'
-
